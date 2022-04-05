586bcc647f85b22ce3ee1e86d137e2c26c96124b
Salem Al Haddad <salem.alhaddad@hotmail.com>
2022-04-05 21:27:44 +0400

    dockerized db + clean directory

--- server/index.js
+++ server/index.js
@@ -3,31 +3,11 @@ const app = express();
 const { resolvers } = require("./schemas/resolver");
 const { typeDefs } = require("./schemas/schema");
 const { ApolloServer } = require('apollo-server');
-const { DataStore } = require('notarealdb')
-const { startups } = require("./db");
-const jsonFile = require("./data/startups.json")
-// app.use(cors());
-// app.use(express.json());
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema,
-//     graphiql: true,
-// 	rootValue: resolvers
-//   })
-// );
 
 const server = new ApolloServer({ typeDefs, resolvers });
+
 // Launch the server
 const PORT = process.env.PORT || 5555;
 server.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
-
-
-
-// var port = process.env.PORT || 5555;
-
-// app.listen(port, "0.0.0.0", function() {
-// console.log("Listening on Port 5555");
-// });
